2aa0552d9c7fa4e95082da516d6fcbee481bc98b
paradox-glitch <35745554+paradox-glitch@users.noreply.github.com>
2019-02-03 23:22:58 +0000

    03FEB19-2322

--- Assets/facemouse.cs
+++ Assets/facemouse.cs
@@ -27,66 +27,68 @@ public class facemouse : MonoBehaviour
 
     private void SpriteSystem()
     {
-        if (transform.rotation.z < 0)
+        if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180)
         {
             mySpriteRenderer.flipX = true;
+            Debug.Log("Fliped");
         }
-        else if (transform.rotation.z > 0)
+        else if (transform.eulerAngles.z < 360 && transform.eulerAngles.z > 180)
         {
             mySpriteRenderer.flipX = false;
+            Debug.Log("Unfliped");
         }
 
-        Debug.Log(string.Format("WeaponNum = {0}", transform.rotation.z * 180));
+        //Debug.Log(string.Format("Rotation = {0}", transform.eulerAngles.z));
 
-        if (transform.rotation.z < 7.5 && transform.rotation.z > -7.5)
+        if (transform.eulerAngles.z < 7.5 || transform.eulerAngles.z > 352.5)
         {
             Debug.Log("Sec1");
         }
-        else if (transform.rotation.z < -7.5 && transform.rotation.z > -22.5 || transform.rotation.z > 7.5 && transform.rotation.z < 22.5)
+        else if (transform.eulerAngles.z > 7.5 && transform.eulerAngles.z < 22.5 || transform.eulerAngles.z > 337.5 && transform.eulerAngles.z < 352.5)
         {
             Debug.Log("Sec2");
         }
-        else if (transform.rotation.z < -22.5 && transform.rotation.z > -37.5 || transform.rotation.z > 22.5 && transform.rotation.z < 37.5)
+        else if (transform.eulerAngles.z > 22.5 && transform.eulerAngles.z < 37.5 || transform.eulerAngles.z > 322.5 && transform.eulerAngles.z < 337.5)
         {
             Debug.Log("Sec3");
         }
-        else if (transform.rotation.z < -37.5 && transform.rotation.z > -52.5 || transform.rotation.z > 37.5 && transform.rotation.z < 52.5)
+        else if (transform.eulerAngles.z > 37.5 && transform.eulerAngles.z < 52.5 || transform.eulerAngles.z > 307.5 && transform.eulerAngles.z < 322.5)
         {
             Debug.Log("Sec4");
         }
-        else if (transform.rotation.z < -52.5 && transform.rotation.z > -67.5 || transform.rotation.z > 52.5 && transform.rotation.z < 67.5)
+        else if (transform.eulerAngles.z > 52.5 && transform.eulerAngles.z < 67.5 || transform.eulerAngles.z > 292.5 && transform.eulerAngles.z < 307.5)
         {
             Debug.Log("Sec5");
         }
-        else if (transform.rotation.z < -67.5 && transform.rotation.z > -82.5 || transform.rotation.z > 67.5 && transform.rotation.z < 82.5)
+        else if (transform.eulerAngles.z > 67.5 && transform.eulerAngles.z < 82.5 || transform.eulerAngles.z > 277.5 && transform.eulerAngles.z < 292.5)
         {
             Debug.Log("Sec6");
         }
-        else if (transform.rotation.z < -82.5 && transform.rotation.z > -97.5 || transform.rotation.z > 82.5 && transform.rotation.z < 97.5)
+        else if (transform.eulerAngles.z > 82.5 && transform.eulerAngles.z < 97.5 || transform.eulerAngles.z > 262.5 && transform.eulerAngles.z < 277.5)
         {
             Debug.Log("Sec7");
         }
-        else if (transform.rotation.z < -97.5 && transform.rotation.z > -112.5 || transform.rotation.z > 97.5 && transform.rotation.z < 112.5)
+        else if (transform.eulerAngles.z > 97.5 && transform.eulerAngles.z < 112.5 || transform.eulerAngles.z > 247.5 && transform.eulerAngles.z < 262.5)
         {
             Debug.Log("Sec8");
         }
-        else if (transform.rotation.z < -112.5 && transform.rotation.z > -127.5 || transform.rotation.z > 112.5 && transform.rotation.z < 127.5)
+        else if (transform.eulerAngles.z > 112.5 && transform.eulerAngles.z < 127.5 || transform.eulerAngles.z > 232.5 && transform.eulerAngles.z < 247.5)
         {
             Debug.Log("Sec9");
         }
-        else if (transform.rotation.z < -127.5 && transform.rotation.z > -142.5 || transform.rotation.z > 127.5 && transform.rotation.z < 142.5)
+        else if (transform.eulerAngles.z > 127.5 && transform.eulerAngles.z < 142.5 || transform.eulerAngles.z > 217.5 && transform.eulerAngles.z < 232.5)
         {
             Debug.Log("Sec10");
         }
-        else if (transform.rotation.z < -142.5 && transform.rotation.z > -157.5 || transform.rotation.z > 142.5 && transform.rotation.z < 157.5)
+        else if (transform.eulerAngles.z > 142.5 && transform.eulerAngles.z < 157.5 || transform.eulerAngles.z > 202.5 && transform.eulerAngles.z < 217.5)
         {
             Debug.Log("Sec11");
         }
-        else if (transform.rotation.z < -157.5 && transform.rotation.z > -172.5 || transform.rotation.z > 157.5 && transform.rotation.z < 172.5)
+        else if (transform.eulerAngles.z > 157.5 && transform.eulerAngles.z < 172.5 || transform.eulerAngles.z > 187.5 && transform.eulerAngles.z < 202.5)
         {
             Debug.Log("Sec12");
         }
-        else if (transform.rotation.z < -172.5 || transform.rotation.z > 172.5)
+        else if (transform.eulerAngles.z > 172.5 && transform.eulerAngles.z < 187.5)
         {
             Debug.Log("Sec13");
         }
