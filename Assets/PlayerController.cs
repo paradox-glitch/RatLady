499e946f5ee1f3ba054579efbab1bc0bbb45cc0d
paradox-glitch <35745554+paradox-glitch@users.noreply.github.com>
2019-02-03 21:42:50 +0000

    03FEB19-2141

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;      
+
+    float moveVertical, moveHorizontal;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+
+        moveVertical = Input.GetAxis("Vertical");
+    }
+    private void Update()
+    {
+        if (moveVertical > 0)
+        {
+            rb2d.AddForce(transform.up * 2);
+        }
+        else if (moveVertical < 0)
+        {
+            rb2d.AddForce(transform.up * -2);
+        }
+
+
+        if (moveHorizontal > 0)
+        {
+            rb2d.AddForce(transform.right * 2);
+        }
+        else if (moveHorizontal < 0)
+        {
+            rb2d.AddForce(transform.right * -2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.angularVelocity = 0;
+        }
+    }
+}
---
+++ Assets/facemouse.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class facemouse : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        FaceMouseSystem();
+        SpriteSystem();
+    }
+
+    private void FaceMouseSystem()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+    }
+
+    private void SpriteSystem()
+    {
+        if (transform.rotation.z < 0)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else if (transform.rotation.z > 0)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+
+        Debug.Log(string.Format("WeaponNum = {0}", transform.rotation.z * 180));
+
+        if (transform.rotation.z < 7.5 && transform.rotation.z > -7.5)
+        {
+            Debug.Log("Sec1");
+        }
+        else if (transform.rotation.z < -7.5 && transform.rotation.z > -22.5 || transform.rotation.z > 7.5 && transform.rotation.z < 22.5)
+        {
+            Debug.Log("Sec2");
+        }
+        else if (transform.rotation.z < -22.5 && transform.rotation.z > -37.5 || transform.rotation.z > 22.5 && transform.rotation.z < 37.5)
+        {
+            Debug.Log("Sec3");
+        }
+        else if (transform.rotation.z < -37.5 && transform.rotation.z > -52.5 || transform.rotation.z > 37.5 && transform.rotation.z < 52.5)
+        {
+            Debug.Log("Sec4");
+        }
+        else if (transform.rotation.z < -52.5 && transform.rotation.z > -67.5 || transform.rotation.z > 52.5 && transform.rotation.z < 67.5)
+        {
+            Debug.Log("Sec5");
+        }
+        else if (transform.rotation.z < -67.5 && transform.rotation.z > -82.5 || transform.rotation.z > 67.5 && transform.rotation.z < 82.5)
+        {
+            Debug.Log("Sec6");
+        }
+        else if (transform.rotation.z < -82.5 && transform.rotation.z > -97.5 || transform.rotation.z > 82.5 && transform.rotation.z < 97.5)
+        {
+            Debug.Log("Sec7");
+        }
+        else if (transform.rotation.z < -97.5 && transform.rotation.z > -112.5 || transform.rotation.z > 97.5 && transform.rotation.z < 112.5)
+        {
+            Debug.Log("Sec8");
+        }
+        else if (transform.rotation.z < -112.5 && transform.rotation.z > -127.5 || transform.rotation.z > 112.5 && transform.rotation.z < 127.5)
+        {
+            Debug.Log("Sec9");
+        }
+        else if (transform.rotation.z < -127.5 && transform.rotation.z > -142.5 || transform.rotation.z > 127.5 && transform.rotation.z < 142.5)
+        {
+            Debug.Log("Sec10");
+        }
+        else if (transform.rotation.z < -142.5 && transform.rotation.z > -157.5 || transform.rotation.z > 142.5 && transform.rotation.z < 157.5)
+        {
+            Debug.Log("Sec11");
+        }
+        else if (transform.rotation.z < -157.5 && transform.rotation.z > -172.5 || transform.rotation.z > 157.5 && transform.rotation.z < 172.5)
+        {
+            Debug.Log("Sec12");
+        }
+        else if (transform.rotation.z < -172.5 || transform.rotation.z > 172.5)
+        {
+            Debug.Log("Sec13");
+        }
+    }
+}
